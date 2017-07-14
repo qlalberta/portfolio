5f06d996c60d0aa233ff53a18aa0c567d5d2e6d9
ql24 <qlalberta@gmail.com>
2017-07-14 10:02:56 -0700

    finished to show selected section on main nav

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,10 +1,16 @@
 'use strict';
 
-articleView.handleMainNav = function() {
-  $('.main-nav .tab').on('click', function() {
+var projectView = {};
+projectView.handleMainNav = function() {
+  console.log(5);
+  $('.tab').on('click', function() {
+    console.log(7);
     $('.tab-content').hide();
     $('#' + $(this).attr('data-content')).show();
   });
 
   $('.main-nav .tab:first').click();
 };
+
+projectView.handleMainNav();
+console.log(projectView);
