70cc10d444f6c72291593063429dccb09107cd62
ql24 <qlalberta@gmail.com>
2017-07-28 16:10:55 -0700

    modified routes.js

--- public/scripts/controller/routes.js
+++ public/scripts/controller/routes.js
@@ -4,8 +4,8 @@ page('/', app.homeController.setNavMenu);
 page('/about', app.aboutController.setNavMenu);
 page('/skills', app.skillController.setNavMenu);
 page('/projects', app.projectController.setNavMenu);
-page('/repos', app.repoController.loadRepos);
-page('/repos/:name', app.getRepos, app.reposController.findRepo, app.reposController.index)
+page('/repos', app.getRepos, app.repoController.index);
+page('/repos/:name', app.getRepos, app.repoController.findRepo, app.repoController.index)
 
 
 page();
